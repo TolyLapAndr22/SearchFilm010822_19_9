8e7e99423dc637fb66063c8f9e25f16f1d1606e6
aalapandr22 <aalap73@mail.ru>
2022-08-03 20:51:28 +0300

    24_7 DetailsActivity12

--- app/src/main/java/com/example/searchfilm010822_19_9/FilmListRecyclerAdapter.kt
+++ app/src/main/java/com/example/searchfilm010822_19_9/FilmListRecyclerAdapter.kt
@@ -4,7 +4,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.searchfilm010822_19_9.Film
 import com.example.searchfilm010822_19_9.FilmViewHolder
 import com.example.searchfilm010822_19_9.R
-import kotlinx.android.synthetic.main.film_item.view.*
 
 //в параметр передаем слушатель, чтобы мы потом могли обрабатывать нажатия из класса Activity
 class FilmListRecyclerAdapter(private val clickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -23,15 +22,7 @@ class FilmListRecyclerAdapter(private val clickListener: OnItemClickListener) : 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is FilmViewHolder -> {
-//Вызываем метод bind(), который мы создали, и передаем туда объект
-                //из нашей базы данных с указанием позиции
-                holder.bind(items[position])
-                //Обрабатываем нажатие на весь элемент целиком(можно сделать на отдельный элемент
-                //например, картинку) и вызываем метод нашего листенера, который мы получаем из
-                //конструктора адаптера
-                holder.itemView.item_container.setOnClickListener {
-                    clickListener.click(items[position])
-                }
+
             }
         }
     }
