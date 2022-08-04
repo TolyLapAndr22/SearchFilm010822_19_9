f0ebf1d8327a2af405ea8f2c98404e3deed3603f
aalapandr22 <aalap73@mail.ru>
2022-08-04 11:04:11 +0300

    modul25_7. fragment_home

---
+++ app/src/main/java/com/example/searchfilm010822_19_9/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.searchfilm010822_19_9
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.content.Intent
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
